70e802b37cfad88b37fc5ae6ff37e59674593983
jackzil <35060795+jackzil@users.noreply.github.com>
2018-10-08 04:07:33 +0700

    Add files via upload

---
+++ chaust.php
@@ -0,0 +1,7 @@
+<?php
+//masukan config token yg terdapat pada link di packet captur
+// jangan hapus tanda kutip(" ") 
+
+$access_token = "3def6286-de1c-4776-a96a-0b73a26cb43d";
+
+// kamu bisa membuat banyak config untuk beberapa akun.
