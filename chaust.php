e58aab1810c3d5a7212523422fa20727f25f1d3a
jackzil <35060795+jackzil@users.noreply.github.com>
2018-10-07 22:10:47 +0000

    Update chaust.php

--- chaust.php
+++ chaust.php
@@ -2,6 +2,6 @@
 //masukan config token yg terdapat pada link di packet captur
 // jangan hapus tanda kutip(" ") 
 
-$access_token = "3def6286-de1c-4776-a96a-0b73a26cb43d";
+$access_token = "c49da6cf-56ea-46e8-b040-20936eda5b8a"
 
 // kamu bisa membuat banyak config untuk beberapa akun.
